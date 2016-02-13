69bf24be6928d5440406c6c6ccd3116c24574e52
rymcmahon <toedragdangle9@gmail.com>
2016-02-13 07:07:28 -0600

    added show action and page

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,5 +1,23 @@
 class ClientsController < ApplicationController
-	def new
+  
+  def show
+    @client = Client.find(params[:id])
+  end
 
-	end
+  def new
+    @client = Client.new
+  end
+
+  def create
+    @client = Client.new(client_params)
+
+    @client.save
+    redirect_to @client  
+  end
+      
+  private
+  
+    def client_params
+      params.require(:client).permit(:first_name, :last_name, :middle_initial, :gender, :date_of_birth, :height, :weight, :address, :home_phone)
+    end
 end
