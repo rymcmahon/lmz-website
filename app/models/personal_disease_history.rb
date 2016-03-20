343d65dd11bdc3c3419d85859d2a29230583f64e
rymcmahon <toedragdangle9@gmail.com>
2016-03-20 17:05:36 -0500

    added validations to personal and family disease histories and fixed show columns

--- app/models/personal_disease_history.rb
+++ app/models/personal_disease_history.rb
@@ -1,3 +1,4 @@
 class PersonalDiseaseHistory < ActiveRecord::Base
   belongs_to :client
+  validates :heart_disease, :cancer, :diabetes, :hypertension, :thyroid_disorder, :hepatitis, :asthma_allergies, :seizures, :stroke, :neurological_disorders, :substance_abuse, presence: true
 end
